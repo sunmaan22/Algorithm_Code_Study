ddb52bda373f136751672d0fd1c800045b12d8f4
SUNMA4N <sunmaan22@gmail.com>
2026-09-06 15:01:13 +0900

    [Passed] 특정 위치의 수 (2ms, 0MB)

---
+++ trail1/특정 위치의 수/numbers-in-specific-location.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[10];
+    for(int i=0;i<10;++i){
+        cin>>arr[i];
+    }
+    cout<<arr[2]+arr[4]+arr[9];
+    // Please write your code here.
+    return 0;
+}
