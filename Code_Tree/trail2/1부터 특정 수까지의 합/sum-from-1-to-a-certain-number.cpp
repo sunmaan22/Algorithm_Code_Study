2f97a4944996fe5acd3dc1d998cc61eaa4408320
SUNMA4N <sunmaan22@gmail.com>
2026-09-11 13:21:14 +0900

    [Passed] 1부터 특정 수까지의 합 (2ms, 0MB)

---
+++ Code_Tree/trail2/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int num(int N){
+    int sum=0;
+    for(int i=1;i<=N;++i){
+        sum += i;
+    }
+    return sum/10;
+}
+
+int main() {
+    cin >> N;
+    cout<<num(N);
+
+    // Please write your code here.
+
+    return 0;
+}
