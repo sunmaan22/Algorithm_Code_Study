2136d442a0e8e4aa33de44678770a246f4c76a67
SUNMA4N <sunmaan22@gmail.com>
2026-09-11 13:24:04 +0900

    [Passed] 정수의 최솟값 (2ms, 0MB)

---
+++ Code_Tree/trail2/정수의 최솟값/minimum-value-of-an-integer.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int a, b, c;
+
+int main() {
+    cin >> a >> b >> c;
+    cout<<min(c,min(a,b));
+
+    // Please write your code here.
+
+    return 0;
+}
